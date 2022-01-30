78f42f5b8f46e7d3cf4cee3d28c9f375666f2df1
greensky0526 <dbswovlf2009@naver.com>
2022-01-30 23:44:24 +0900

    마무리
    ArrayList -> ArrayOf
    
    click event를 위해 context 획득

--- app/src/main/java/com/greensky0526/recyclerviewpractice/PhoneBookListAdapter.kt
+++ app/src/main/java/com/greensky0526/recyclerviewpractice/PhoneBookListAdapter.kt
@@ -1,21 +1,43 @@
 package com.greensky0526.recyclerviewpractice
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class PhoneBookListAdapter(var persons: ArrayList<Person>) :
+class PhoneBookListAdapter(var persons: Array<Person>, var con: Context) :
     RecyclerView.Adapter<PhoneBookListAdapter.ViewHolder>() {
     var TAG = "PhoneBookListAdapter"
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var iv_person_phone_book_list_item: ImageView = itemView.findViewById(R.id.iv_person_phone_book_list_item)
-        var tv_name_phone_book_list_item: TextView = itemView.findViewById(R.id.tv_name_phone_book_list_item)
-        var tv_phone_number_phone_book_list_item: TextView = itemView.findViewById(R.id.tv_phone_number_phone_book_list_item)
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        lateinit var iv_person_phone_book_list_item: ImageView
+        lateinit var tv_name_phone_book_list_item: TextView
+        lateinit var tv_phone_number_phone_book_list_item: TextView
+
+        init {
+            iv_person_phone_book_list_item = itemView.findViewById(R.id.iv_person_phone_book_list_item)
+            tv_name_phone_book_list_item = itemView.findViewById(R.id.tv_name_phone_book_list_item)
+            tv_phone_number_phone_book_list_item = itemView.findViewById(R.id.tv_phone_number_phone_book_list_item)
+
+            itemView.setOnClickListener {
+                AlertDialog.Builder(con).apply {
+                    var position = adapterPosition
+                    var person = persons[position]
+                    setTitle(person.name)
+                    setMessage(person.phoneNumber)
+                    setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(con, "OK Button Click", Toast.LENGTH_SHORT).show()
+                    })
+                    show()
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -27,7 +49,7 @@ class PhoneBookListAdapter(var persons: ArrayList<Person>) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val person: Person = persons.get(position)
+        val person: Person = persons[position]
         //[수정요함] 이미지 작업의 경우 glide를 사용해 server의 image를 불러올 것
         //holder.iv_person_phone_book_list_item
         holder.tv_name_phone_book_list_item.text = person.name
@@ -37,4 +59,6 @@ class PhoneBookListAdapter(var persons: ArrayList<Person>) :
     override fun getItemCount(): Int {
         return persons.size
     }
+
+
 }
