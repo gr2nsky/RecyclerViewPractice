be18a343577b18608d2e72efdd8beb5cd3bf6198
greensky0526 <dbswovlf2009@naver.com>
2022-02-03 23:37:50 +0900

    검색기능 완성

--- app/src/main/java/com/greensky0526/recyclerviewpractice/MainActivity.kt
+++ app/src/main/java/com/greensky0526/recyclerviewpractice/MainActivity.kt
@@ -1,25 +1,49 @@
 package com.greensky0526.recyclerviewpractice
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.SearchView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
 
     lateinit var rv_phone_book: RecyclerView
     lateinit var phoneBookListAdapter: PhoneBookListAdapter
-    lateinit var persons:Array<Person>
+    lateinit var persons: ArrayList<Person>
+
+    lateinit var search_view_phone_book: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         rv_phone_book = findViewById(R.id.rv_phone_book)
+        search_view_phone_book = findViewById(R.id.search_view_phone_book)
+
+        search_view_phone_book.setOnQueryTextListener(searchViewTextListener)
 
         persons = tempPersons()
         setAdapter()
     }
+
+    //SearchView 텍스트 입력시 이벤트
+    var searchViewTextListener: SearchView.OnQueryTextListener =
+        object : SearchView.OnQueryTextListener {
+            //검색버튼 입력시 호출, 검색버튼이 없으므로 사용하지 않음
+            override fun onQueryTextSubmit(s: String): Boolean {
+                return false
+            }
+
+            //텍스트 입력/수정시에 호출
+            override fun onQueryTextChange(s: String): Boolean {
+                phoneBookListAdapter.getFilter().filter(s)
+                Log.d(TAG, "SearchVies Text is changed : $s")
+                return false
+            }
+        }
 
     fun setAdapter(){
         //리사이클러뷰에 리사이클러뷰 어댑터 부착
@@ -28,17 +52,17 @@ class MainActivity : AppCompatActivity() {
         rv_phone_book.adapter = phoneBookListAdapter
     }
 
-    fun tempPersons(): Array<Person> {
-        return arrayOf(
-            Person(1, "kim", "01011111111"),
-            Person(2, "lee", "01022222222"),
-            Person(3, "park", "01033333333"),
-            Person(4, "son", "01044444444"),
-            Person(5, "hwang", "01055555555"),
-            Person(6, "jo", "01066666666"),
-            Person(7, "gwak", "01077777777"),
-            Person(8, "sim", "01088888888"),
-            Person(9, "choi", "01099999999"),
-        )
+    fun tempPersons(): ArrayList<Person> {
+        var tempPersons = ArrayList<Person>()
+        tempPersons.add(Person(1, "kim", "01011111111"))
+        tempPersons.add(Person(2, "lee", "01022222222"))
+        tempPersons.add(Person(3, "park", "01033333333"))
+        tempPersons.add(Person(4, "son", "01044444444"))
+        tempPersons.add(Person(5, "hwang", "01055555555"))
+        tempPersons.add(Person(6, "jo", "01066666666"))
+        tempPersons.add(Person(7, "gwak", "01077777777"))
+        tempPersons.add(Person(8, "sim", "01088888888"))
+        tempPersons.add(Person(9, "choi", "01099999999"))
+        return tempPersons
     }
 }
--- app/src/main/java/com/greensky0526/recyclerviewpractice/PhoneBookListAdapter.kt
+++ app/src/main/java/com/greensky0526/recyclerviewpractice/PhoneBookListAdapter.kt
@@ -1,19 +1,23 @@
 package com.greensky0526.recyclerviewpractice
 
+import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.content.Context
 import android.content.DialogInterface
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
+import android.widget.Filter.FilterResults
 import androidx.recyclerview.widget.RecyclerView
 
-class PhoneBookListAdapter(var persons: Array<Person>, var con: Context) :
-    RecyclerView.Adapter<PhoneBookListAdapter.ViewHolder>() {
+class PhoneBookListAdapter(var persons: ArrayList<Person>, var con: Context) :
+    RecyclerView.Adapter<PhoneBookListAdapter.ViewHolder>(), Filterable {
     var TAG = "PhoneBookListAdapter"
+
+    var filteredPersons = ArrayList<Person>()
+    var itemFilter = ItemFilter()
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var iv_person_phone_book_list_item: ImageView
@@ -28,7 +32,7 @@ class PhoneBookListAdapter(var persons: Array<Person>, var con: Context) :
             itemView.setOnClickListener {
                 AlertDialog.Builder(con).apply {
                     var position = adapterPosition
-                    var person = persons[position]
+                    var person = filteredPersons[position]
                     setTitle(person.name)
                     setMessage(person.phoneNumber)
                     setPositiveButton("OK", DialogInterface.OnClickListener { dialog, which ->
@@ -40,6 +44,10 @@ class PhoneBookListAdapter(var persons: Array<Person>, var con: Context) :
         }
     }
 
+    init {
+        filteredPersons.addAll(persons)
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val con = parent.context
         val inflater = con.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -49,7 +57,7 @@ class PhoneBookListAdapter(var persons: Array<Person>, var con: Context) :
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val person: Person = persons[position]
+        val person: Person = filteredPersons[position]
         //[수정요함] 이미지 작업의 경우 glide를 사용해 server의 image를 불러올 것
         //holder.iv_person_phone_book_list_item
         holder.tv_name_phone_book_list_item.text = person.name
@@ -57,7 +65,60 @@ class PhoneBookListAdapter(var persons: Array<Person>, var con: Context) :
     }
 
     override fun getItemCount(): Int {
-        return persons.size
+        return filteredPersons.size
+    }
+
+    //-- filter
+
+    fun initFilter() {
+        filteredPersons = persons
+    }
+
+    override fun getFilter(): Filter {
+        return itemFilter
+    }
+
+    inner class ItemFilter : Filter() {
+        override fun performFiltering(charSequence: CharSequence): FilterResults {
+            val filterString = charSequence.toString()
+            val results = FilterResults()
+            Log.d(TAG, "charSequence : $charSequence")
+
+            //검색조건이 추가됬으므로, 검색이 필요없을 경우를 미리 처리
+            val filteredList: ArrayList<Person> = ArrayList<Person>()
+
+            if (filterString.trim { it <= ' ' }.isEmpty()) {
+                results.values = persons
+                results.count = persons.size
+                return results
+            } else if (filterString.trim { it <= ' ' }.length <= 2) {
+                for (person in persons) {
+                    if (person.name.contains(filterString)) {
+                        filteredList.add(person)
+                    }
+                }
+                results.values = filteredList
+                results.count = filteredList.size
+            } else {
+                for (person in persons) {
+                    if (person.name.contains(filterString) ||
+                        person.phoneNumber.contains(filterString)
+                    ) {
+                        filteredList.add(person)
+                    }
+                }
+                results.values = filteredList
+                results.count = filteredList.size
+            }
+            return results
+        }
+
+        @SuppressLint("NotifyDataSetChanged")
+        override fun publishResults(charSequence: CharSequence?, filterResults: FilterResults) {
+            filteredPersons.clear()
+            filteredPersons.addAll(filterResults.values as ArrayList<Person>)
+            notifyDataSetChanged()
+        }
     }
 
 
